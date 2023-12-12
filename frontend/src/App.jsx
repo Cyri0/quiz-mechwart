199af2f8104fe68f5320be1fe32eb9c3f1ac1a49
Nemes Tamás <nemes.tamas@mechwart.local>
2023-12-12 16:12:30 +0100

    some changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from 'react'
 
 const App = () => {
-  const URL =  "http://127.0.0.1:8000/api/"
+  const URL =  "/api/"
   const [questions, setQuestions] = useState([])
   const [number, setNumber] = useState(0)
 
@@ -10,6 +10,20 @@ const App = () => {
     .then(res => res.json())
     .then(data => setQuestions(data))
   },[])
+
+  const testMyAnswer = (selected,e) => {
+    if(questions[number][selected])
+      {
+        console.log("Jó válasz!");
+        e.target.style.backgroundColor = "green"
+      
+      }
+      else{
+        console.log("Hibás válasz!");
+        e.target.style.backgroundColor = "red"
+
+      }
+  }
 
   return (
     <div className='container'>
@@ -21,12 +35,13 @@ const App = () => {
       <h1>Loading..</h1> :
       <>
         <h1>{questions[number].question}</h1>
-        <button className="answer">{questions[number].answer1}</button>
-        <button className="answer">{questions[number].answer2}</button>
-        <button className="answer">{questions[number].answer3}</button>
-        <button className="answer">{questions[number].answer4}</button>
 
-        <button className="next">Következő</button>
+        <button onClick={(e)=>testMyAnswer("answer1_is_true",e)} className="answer">{questions[number].answer1}</button>
+        <button onClick={(e)=>testMyAnswer("answer2_is_true",e)} className="answer">{questions[number].answer2}</button>
+        <button onClick={(e)=>testMyAnswer("answer3_is_true",e)} className="answer">{questions[number].answer3}</button>
+        <button onClick={(e)=>testMyAnswer("answer4_is_true",e)} className="answer">{questions[number].answer4}</button>
+
+        <button className="next" onClick={()=>{setNumber(number => number += 1)}}>Következő</button>
       </>
       }
     </div>
